d208cf17cf4e8e5cf67e14d1e2a24b0205249245
Della Dominic <delladominic33@gmail.com>
2025-08-30 19:59:46 +0530

    Add vite configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,9 +26,4 @@ export default [
   prettier, // make sure to put prettier at the end so it can override other conflicting eslint rules or configs
   //   If you put prettier before other configs, those configs might re-enable rules that Prettier disables, causing formatting conflicts and unnecessary lint errors.
 ];
-
-// note:
-// npm run lint -- --fix
-// here the -- will attach the flag, --fix to the lint script command in package.json and not to the npm command itself.
-// without --fix it will just show the lint errors, with --fix it will fix the errors wherever possible.
-// you can also do npx eslint . --fix
+// https://eslint.org/docs/latest/use/configure/configuration-files
